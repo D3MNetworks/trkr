daf9694079de5130da474dcfd843d4269cc142e3
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2017-11-10 19:13:33 -0500

    TRKR with Trello and Sheets integrations

---
+++ fill_row.py
@@ -0,0 +1,118 @@
+import datetime
+from collections import namedtuple
+
+import os
+import configparser
+
+import pygsheets
+from trello import TrelloClient
+from pick import pick
+
+########## Conveniance methods
+def search_trello():
+  query = input("[trello] Search term(s): ")
+  if not query: return
+
+  cards = trello_client.search(
+    query,
+    True,
+    ["cards"],
+    [config["trello_board_id"]]
+  )
+  return pick_card(cards)
+
+def filter_trello():
+  board = trello_client.get_board(config["trello_board_id"])
+  cards = board.get_cards(
+    None,
+    "visible"
+  )
+  cards = filter(lambda x: config["trello_id"] in x.member_ids, cards)
+  return pick_card(list(cards), "Select from your cards")
+
+def pick_card(cards, title="Select card"):
+  title = "[trello] " + title
+  options = list(map((lambda c: c.name), cards))
+  option, index = pick(options, title)
+  return cards[index].short_url
+
+def validate_hours():
+  while True:
+    try:
+      hrs = float(input("Hours spent: "))
+    except ValueError:
+      print("Format Error: Must be a number")
+      continue
+    else:
+      return hrs
+      break
+
+def validate_date():
+  while True:
+    try:
+      date_text = input("Date (MM/DD/YYYY): ")
+      if date_text:
+        datetime.datetime.strptime(date_text, "%m/%d/%Y")
+    except ValueError:
+      print("Format Error: Must be formatted as MM/DD/YYYY")
+      continue
+    else:
+      return date_text
+      break
+##########
+
+# Config and Setup
+config = configparser.ConfigParser()
+home = os.path.expanduser("~")
+config_path = os.path.join(home, ".trkr/config.ini")
+config.read(config_path)
+
+trello_config = config["trello"]
+sheet_config = config["sheets"]
+email = config["default"]["email"]
+
+# Authorize API endpoints
+trello_client = TrelloClient(
+  api_key=trello_config.get("api_key"),
+  api_secret=trello_config.get("api_secret"),
+  token=trello_config.get("token"),
+)
+
+keyfile = os.path.join(home, ".trkr/keyfile.json")
+ps = pygsheets.authorize(service_file=keyfile)
+wks = ps.open_by_url(sheet_config.get("url")).worksheet_by_title(sheet_config.get("wks_name"))
+
+# Get current datetime
+now = datetime.datetime.now()
+timestamp = now.strftime("%m/%d/%Y %H:%M:%S")
+
+# Start requesting user input
+description = input("Description of work: ")
+hours = validate_hours()
+
+# Options for trello card selection
+trello = input("[trello] Input/Search/Pick/None [i/s/p/n]: ")
+if trello == "i":
+  card_name = input("[trello] Card URL: ")
+elif trello == "s":
+  card_name = search_trello()
+elif trello == "p":
+  card_name = filter_trello()
+else:
+  card_name = ""
+
+date = validate_date() or now.strftime("%m/%d/%Y")
+
+wks.insert_rows(wks.rows - 1, 1, [
+  timestamp,
+  date,
+  description,
+  hours,
+  card_name,
+  email
+], True)
+
+# Also write to trkr journal
+journal = open(os.path.join(home, ".trkr/journal.csv"), "a+")
+journal.write(f"\"{timestamp},{date},{description},{hours},{card_name},{email}\"\n")
+journal.close()
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+import configparser
+
+config = configparser.ConfigParser()
+home = os.path.expanduser("~")
+
+if not os.path.exists(os.path.join(home, ".trkr")):
+    os.makedirs(os.path.join(home, ".trkr"))
+
+config_path = os.path.join(home, ".trkr/config.ini")
+
+config["default"] = {
+  "email": input("Email: ")
+}
+
+print("\nTrello Setup\n")
+
+config["trello"] = {
+  "user_id": input("Client ID: "),
+  "board_id": input("Board ID: "),
+  "api_key": input("API Key: "),
+  "api_secret": input("API Secret: "),
+  "token": input("Token: ")
+}
+
+print("\nGoogle Sheets Setup\n")
+
+config["sheets"] = {
+  "url": input("Document URL: "),
+  "wks_name": input("Worksheet Name: ")
+}
+
+print("Generate/ask for a keyfile and move it to ~/.trkr/keyfile.json")
+input("Press ENTER when completed")
+
+with open(config_path, "w+") as configfile:
+  config.write(configfile)
